5dbc8739066570f11f9f3367538b297084b0f9eb
Wiz Lee <Wiz.Lee@grassvalley.com>
2022-03-05 00:52:32 +0800

    Complete frontend without CSS styling
    - Pending backend work

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NgxStripeModule } from 'ngx-stripe';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgxStripeModule.forRoot('pk_test_51KYAK5CjaVImKg1qOxArglL5eSddRgsex6Nns8P9nH6iUoXcdjZxVJN0QQfI3GZG4WQMmELlBpkmeGtJU10WQP6g00F5fsVQfa'),
   ],
   providers: [],
   bootstrap: [AppComponent]
